74faa8ff2267818d70699adea44e5a0c27896aab
RyumaNoma <noma11.develop@gmail.com>
2023-05-18 11:19:07 +0900

    [clean]Randomのコメントを追加

--- ReversiConsole/src/Random.hpp
+++ ReversiConsole/src/Random.hpp
@@ -7,8 +7,17 @@ public:
 	Random(std::uint64_t seed);
 	Random(const Random&);
 	~Random() {}
-
+	/// <summary>
+	/// ®”—”‚ğ¶¬‚·‚é
+	/// </summary>
+	/// <returns>—”</returns>
 	std::uint32_t Generate();
+
+	/// <summary>
+	/// [0, max)‚Ì”ÍˆÍ‚Å®”—”‚ğ¶¬‚·‚é
+	/// </summary>
+	/// <param name="max">¶¬‚·‚é—”‚ÌãŒÀ</param>
+	/// <returns>—”</returns>
 	std::uint32_t Generate(std::uint32_t max);
 private:
 	std::uint32_t x_;
