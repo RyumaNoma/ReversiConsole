043f9d99780dcc4eb630bfc73112d8f72ed71f17
RyumaNoma <noma11.develop@gmail.com>
2023-05-20 17:16:49 +0900

    [add]HumanをGUIに対応

--- ReversiConsole/src/Human.cpp
+++ ReversiConsole/src/Human.cpp
@@ -1,6 +1,8 @@
 #include "Human.hpp"
 #include <iostream>
 #include "Point/Point.hpp"
+#include "BitBoard.hpp"
+#include "DxLib.h"
 
 void Human::Init()
 {
@@ -8,11 +10,33 @@ void Human::Init()
 
 Point Human::Act(const BitBoard& board)
 {
-	int x, y;
-	std::cout << "X: ";
-	std::cin >> x;
-	std::cout << "Y: ";
-	std::cin >> y;
+	// ‰æ–ÊƒTƒCƒY‚ÌŽæ“¾
+	RECT rc;
+	GetWindowRect(GetDesktopWindow(), &rc);
+	//int window_width = rc.right - rc.left;
+	int window_height = rc.bottom - rc.top;
 
-	return Point(x, y);
+	const int mergin = window_height * 0.1;
+	const int board_size = window_height * 0.8;
+	const int cell_size = board_size / 8;
+
+	std::uint64_t legal = board.LegalPublic();
+	while (true)
+	{
+		int mouse_input = GetMouseInput();
+		if (mouse_input & MOUSE_INPUT_LEFT)
+		{
+			int mouse_x, mouse_y;
+			GetMousePoint(&mouse_x, &mouse_y);
+			
+			if (mouse_x < mergin || mergin + board_size < mouse_x) continue;
+			if (mouse_y < mergin || mergin + board_size < mouse_y) continue;
+
+			int x = (mouse_x - mergin) / cell_size;
+			int y = (mouse_y - mergin) / cell_size;
+			int hash = y * 8 + x;
+			if ((legal & (1ULL << hash)) == 0) continue;
+			return Point(x, y);
+		}
+	}
 }
