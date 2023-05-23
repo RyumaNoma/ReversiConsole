d3db0bd394ea76e2742275e5a0078e4e9583b4c9
RyumaNoma <noma11.develop@gmail.com>
2023-05-23 21:53:29 +0900

    [impl]ResultSceneのとりあえず実装

--- ReversiConsole/src/ResultScene.cpp
+++ ReversiConsole/src/ResultScene.cpp
@@ -2,22 +2,58 @@
 #include "SceneManager.hpp"
 #include "DxLib.h"
 
+ResultScene::ResultScene(SceneManager* manager)
+	: level_(0)
+	, human_result_(0)
+	, human_stones_(0)
+	, ai_stones_(0)
+	, Scene(manager)
+{
+}
+
 void ResultScene::OnChanged(const std::map<std::string, int>& param)
 {
+	level_ = param.at("Level");
+	human_result_ = param.at("HumanResult");
+	human_stones_ = param.at("HumanStones");
+	ai_stones_ = param.at("AIStones");
 }
 
 void ResultScene::Draw() const
 {
-	// プレイヤー？の勝利！
-	// コマの数
-	// 悪手・いい手
 	// Press space to title
+	const int TitleFontSize = 300;
+	const int NormalFontSize = 200;
+	
+	SetFontSize(TitleFontSize);
+	DrawString(0, 0, "Result", GetColor(255, 255, 255));
+	SetFontSize(NormalFontSize);
+	// 勝敗
+	switch (human_result_)
+	{
+	case 1:
+		DrawString(0, TitleFontSize, "You Won!", GetColor(255, 0, 0));
+		break;
+	case 0:
+		DrawString(0, TitleFontSize, "Draw", GetColor(255, 255, 255));
+		break;
+	case -1:
+		DrawString(0, TitleFontSize, "You Lost ...", GetColor(0, 0, 255));
+		break;
+	default:
+		break;
+	}
+	// 石の数
+	DrawFormatString(0, TitleFontSize + NormalFontSize, GetColor(255, 255, 255), "You: %d Stones", human_stones_);
+	DrawFormatString(0, TitleFontSize + NormalFontSize, GetColor(255, 255, 255), " AI: %d Stones", ai_stones_);
+
+	DrawString(0, TitleFontSize + NormalFontSize * 2, "Press space to go to Title", GetColor(255, 255, 255));
 }
 
 void ResultScene::Update()
 {
 	if (CheckHitKey(KEY_INPUT_SPACE))
 	{
-		manager_->Change("Match", std::map<std::string, int>());
+		manager_->Change("Title", std::map<std::string, int>());
 	}
 }
--- ReversiConsole/src/ResultScene.hpp
+++ ReversiConsole/src/ResultScene.hpp
@@ -5,12 +5,16 @@ class ResultScene
 	: public Scene
 {
 public:
-	ResultScene(SceneManager* manager) : Scene(manager) {}
+	ResultScene(SceneManager* manager);
 
 	virtual void OnChanged(const std::map<std::string, int>& param) override;
 
 	virtual void Draw() const override;
 
 	virtual void Update() override;
-
+private:
+	int level_;
+	int human_result_;
+	int human_stones_;
+	int ai_stones_;
 };
