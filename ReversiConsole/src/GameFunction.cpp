ff673083ef31f23621ab83f18fa0b1bab240dec9
RyumaNoma <noma11.develop@gmail.com>
2023-05-24 21:03:03 +0900

    [add]ウィンドウモードの変更用関数の追加

--- ReversiConsole/src/GameFunction.cpp
+++ ReversiConsole/src/GameFunction.cpp
@@ -7,3 +7,18 @@ std::pair<int, int> GameFunction::GetWindowSize()
 	::GetWindowSize(&width, &height);
 	return std::make_pair(width, height);
 }
+
+void GameFunction::ChangeToFullScreenMode()
+{
+	int desktop_w, desktop_h;
+	GetDefaultState(&desktop_w, &desktop_h, NULL);
+	SetGraphMode(desktop_w, desktop_h, 32);
+	ChangeWindowMode(false);
+}
+
+void GameFunction::ChangeToWindowMode(int width, int height)
+{
+	SetGraphMode(width, height, 32);
+	SetWindowSize(width, height);
+	ChangeWindowMode(true);
+}
--- ReversiConsole/src/GameFunction.hpp
+++ ReversiConsole/src/GameFunction.hpp
@@ -9,4 +9,16 @@ public:
 	/// </summary>
 	/// <returns>(横の長さ，縦の長さ)</returns>
 	static std::pair<int, int> GetWindowSize();
+
+	/// <summary>
+	/// ゲームをフルスクリーンモードに変更する
+	/// </summary>
+	static void ChangeToFullScreenMode();
+
+	/// <summary>
+	/// ゲームをウィンドウモードに変更する
+	/// </summary>
+	/// <param name="width">ウィンドウの横幅</param>
+	/// <param name="height">ウィンドウの縦幅</param>
+	static void ChangeToWindowMode(int width, int height);
 };
