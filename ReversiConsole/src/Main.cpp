7e7f58b241475ae4ec5a395e707caf145dd1e317
RyumaNoma <noma11.develop@gmail.com>
2023-05-24 21:03:50 +0900

    [clean]DxLibの実行時初期設定をリファクタした

--- ReversiConsole/src/Main.cpp
+++ ReversiConsole/src/Main.cpp
@@ -11,23 +11,51 @@
 // 汎用
 #include "Point/Point.hpp"
 #include "Timer/Timer.hpp"
+#include "GameFunction.hpp"
 
-
+//DISPLAYMODEDATA ModeData;
+//int i, ModeNum;
+//
+//int DesktopW, DesktopH;
+//GetDefaultState(&DesktopW, &DesktopH, NULL);
+//SetGraphMode(DesktopW, DesktopH, 32);
+//Debug::Print(DesktopW, DesktopH);
+//
+//// ディスプレイモードの数を取得する
+//ModeNum = GetDisplayModeNum();
+//
+//// ディスプレイモードの数だけループする
+//for (i = 0; i < ModeNum; i++)
+//{
+//	// ディスプレイモードの情報を取得する
+//	ModeData = GetDisplayMode(i);
+//
+//	// 画面にディスプレイモードの情報を描画する
+//	DrawFormatString(i / 40 * 128, (i % 40) * 11, GetColor(255, 255, 255), "%dx%d %dbit %dHz",
+//		ModeData.Width, ModeData.Height, ModeData.ColorBitDepth, ModeData.RefreshRate);
+//}
 // プログラムは WinMain から始まります
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	Debug::FILENAME = "./Debug.txt";
 	Debug::Reset();
 
+	// DxLibの実行時初期設定
+	// Windowモード
+	ChangeWindowMode(true);
+	// タイトル
+	SetMainWindowText("Reversi");
+	// カーソルの表示設定：オン
+	SetMouseDispFlag(true);
+	// 初期画面サイズをデスクトップサイズに合わせる
+	GameFunction::ChangeToFullScreenMode();
+	// 背景色の決定
+	SetBackgroundColor(100, 100, 0);
+
 	if (DxLib_Init() == -1)
 	{
 		return -1;
-	}
-	// DxLibの実行時初期設定
-	SetGraphMode(1920, 1080, 32);         // 解像度の設定。人によってモニターが違うから変更できるようにする
-	SetBackgroundColor(100, 100, 0); // 背景色の決定
-	SetDrawArea(0, 0, 1921, 1081);   // 描画可能領域の設定
-	SetMouseDispFlag(true);// カーソルの表示設定：オン
+	}	
 
 	SceneManager manager;
 	TitleScene ts(&manager);
