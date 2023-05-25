e07e61616b075e60891467b18f52a23781c2ba83
RyumaNoma <noma11.develop@gmail.com>
2023-05-25 09:24:19 +0900

    [add]中央ぞろえにした際の左上座標を得る関数を追加

--- ReversiConsole/src/GameFunction.cpp
+++ ReversiConsole/src/GameFunction.cpp
@@ -22,3 +22,16 @@ void GameFunction::ChangeToWindowMode(int width, int height)
 	SetWindowSize(width, height);
 	ChangeWindowMode(true);
 }
+
+std::pair<int, int> GameFunction::GetLeftTopToAlignCenter(int x1, int y1, int x2, int y2, std::string text, int font_size)
+{
+	int width = x2 - x1;
+	int height = y2 - y1;
+	int text_length = static_cast<int>(text.size());
+	std::pair<int, int> left_top;
+
+	left_top.first = (width - GetDrawStringWidth(text.c_str(), text_length)) / 2 + x1;
+	left_top.second = (height - font_size) / 2 + y1;
+
+	return left_top;
+}
--- ReversiConsole/src/GameFunction.hpp
+++ ReversiConsole/src/GameFunction.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <utility>
+#include <string>
 
 class GameFunction
 {
@@ -21,4 +22,16 @@ public:
 	/// <param name="width">ウィンドウの横幅</param>
 	/// <param name="height">ウィンドウの縦幅</param>
 	static void ChangeToWindowMode(int width, int height);
+
+	/// <summary>
+	/// 文字列を中央ぞろえにするための左上の座標を求める．
+	/// </summary>
+	/// <param name="x1">左端の座標</param>
+	/// <param name="y1">上端の座標</param>
+	/// <param name="x2">右端の座標</param>
+	/// <param name="y2">下端の座標</param>
+	/// <param name="text">表示する文字列</param>
+	/// <param name="font_size">表示する際のフォントサイズ</param>
+	/// <returns>左上の座標</returns>
+	static std::pair<int, int> GetLeftTopToAlignCenter(int x1, int y1, int x2, int y2, std::string text, int font_size);
 };
